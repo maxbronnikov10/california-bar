74458115de48643b8bab68f41d6b9764faa2236a
Maxim <maxbronnikov2004@gmail.com>
2022-03-04 01:52:26 +0300

    Set up project
    Add html,sass,img,fonts files

---
+++ js/script.js
@@ -0,0 +1,12 @@
+const progress = document.querySelector('.progress');
+
+window.addEventListener('scroll', progressBar);
+
+function progressBar(event) {
+    let windowScroll = document.body.scrollTop || document.documentElement.scrollTop;
+    let windowHight = document.documentElement.scrollHeight - document.documentElement.clientHeight;
+    let per = windowScroll / windowHight * 100;
+
+    progress.style.width = per + '%';
+
+}
